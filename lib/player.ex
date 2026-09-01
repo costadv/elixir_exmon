3e79f7e06c10ff87a1ea16fbbd8572cd4603ee51
costadv <dvcosta0505@outlook.com>
2026-09-01 14:20:40 -0300

    Created module of player struct and set build function to create any struct.

--- lib/player.ex
+++ lib/player.ex
@@ -1,4 +1,14 @@
 defmodule ExMon.Player do
   @enforce_keys [:name, :life, :moveAvg, :moveRnd, :moveHeal]
   defstruct [:name, :life, :moveAvg, :moveRnd, :moveHeal]
+
+  def build(name, moveAvg, moveRnd, moveHeal) do
+    %ExMon.Player{
+      name: name,
+      life: 100,
+      moveAvg: moveAvg,
+      moveRnd: moveRnd,
+      moveHeal: moveHeal
+  }
+  end
 end
