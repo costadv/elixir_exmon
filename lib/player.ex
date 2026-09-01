24375fe58c7e4b66367cd64b71d563b36647bbaf
costadv <dvcosta0505@outlook.com>
2026-09-01 14:33:01 -0300

    Made changes in player.ex to make it easy to view.

--- lib/player.ex
+++ lib/player.ex
@@ -1,11 +1,14 @@
 defmodule ExMon.Player do
-  @enforce_keys [:name, :life, :moveAvg, :moveRnd, :moveHeal]
-  defstruct [:name, :life, :moveAvg, :moveRnd, :moveHeal]
+  @requiredKeys [:name, :life, :moveAvg, :moveRnd, :moveHeal]
+  @maxLife 100
+
+  @enforce_keys @requiredKeys
+  defstruct @requiredKeys
 
   def build(name, moveAvg, moveRnd, moveHeal) do
     %ExMon.Player{
       name: name,
-      life: 100,
+      life: @maxLife,
       moveAvg: moveAvg,
       moveRnd: moveRnd,
       moveHeal: moveHeal
